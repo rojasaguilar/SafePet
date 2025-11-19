44680bbed7c10cd4c0af843f7aa9e411e371b12d
JesusRobles <jeanrobleslo@ittepic.edu.mx>
2025-11-18 22:58:48 -0700

    Pantallas de Mascotas, Citas y Clínicas

---
+++ safe_pet_client/lib/Citas.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class PantallaCitas extends StatefulWidget {
+  PantallaCitas({super.key});
+
+  @override
+  _CitasScreenState createState() => _CitasScreenState();
+}
+
+class _CitasScreenState extends State<PantallaCitas> {
+  List<Map<String, String>> citas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    citas = [
+      {'mascota': 'Olaf', 'fecha': '2024-10-26 10:00 AM', 'veterinario': 'grapas'},
+      {'mascota': 'Arena', 'fecha': '2024-10-27 11:00 AM', 'veterinario': 'grapas'},
+      {'mascota': 'Megacable Vato', 'fecha': '2024-10-26 02:00 PM', 'veterinario': 'grapas'},
+    ];
+  }
+ //cancelar
+  void _cancelarCita(int index) {
+    setState(() {
+      citas.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Citas"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: citas.length,
+        itemBuilder: (context, index) {
+          final cita = citas[index];
+          return Card(
+            margin: EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Text('Cita para ${cita['mascota']}'),
+              subtitle: Text('Fecha: ${cita['fecha']}\nVeterinario: ${cita['veterinario']}'),
+              trailing: PopupMenuButton<String>(
+                onSelected: (value) {
+                  if (value == 'reagendar') {
+
+                  } else if (value == 'cancelar') {
+                    _cancelarCita(index);
+                  }
+                },
+                itemBuilder: (BuildContext context) {
+                  return {'Reagendar', 'Cancelar'}.map((String choice) {
+                    return PopupMenuItem<String>(
+                      value: choice.toLowerCase(),
+                      child: Text(choice),
+                    );
+                  }).toList();
+                },
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        },
+        child: Icon(Icons.add),
+        tooltip: 'Agendar Cita',
+      ),
+    );
+  }
+}
