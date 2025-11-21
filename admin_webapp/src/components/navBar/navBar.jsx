3bf6a690cb8911e2a174264f4c4cf223c431385d
rojas <rojas.devnet@gmail.com>
2025-11-21 13:59:01 -0700

    webapp dependencias y test @apply

---
+++ admin_webapp/src/components/navBar/navBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function navBar() {
+  return (
+   <>
+   <nav>
+    
+   </nav>
+   </>
+  )
+}
+
+export default navBar
---
+++ admin_webapp/src/components/navBar/navbarCllinicSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function navbarCllinicSection() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default navbarCllinicSection
