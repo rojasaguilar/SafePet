698792c4040e384aae947143f4183b5a4d3b1aab
rojas <rojas.devnet@gmail.com>
2025-11-24 01:35:12 -0600

    pacientes get

--- admin_webapp/src/pages/PacientesPage.jsx
+++ admin_webapp/src/pages/PacientesPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function PacientesPage() {
+  const [mascotas, setMascotas] = useState([]);
+
+  useEffect(() => {
+    const getMascotas = async () => {
+      const { data } = await axios.get("http://localhost:3456/api/v1/mascotas");
+
+      console.log(data.data);
+
+      setMascotas(data.data);
+    };
+
+    getMascotas();
+  }, []);
+
   return (
-    <div>PacientesPage</div>
-  )
+    <>
+      {mascotas.length > 0 ? (
+        <div>
+          {
+            mascotas.map((mascota) => {
+              
+            })
+          }
+        </div>
+      ) : (
+        <div>
+          <h1>Sin mascotas</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default PacientesPage
+export default PacientesPage;
