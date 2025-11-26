2da155cc85ed7642081d3d7073cd5eebfee31e05
rojas <rojas.devnet@gmail.com>
2025-11-25 23:03:52 -0700

    navbar icons

--- admin_webapp/src/components/navBar/navBar.jsx
+++ admin_webapp/src/components/navBar/navBar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import NavbarCllinicSection from './navbarCllinicSection';
 import { Link } from 'react-router-dom';
+import { Home, Users } from 'lucide-react';
 
 function NavBar() {
   return (
@@ -8,9 +8,15 @@ function NavBar() {
       <aside className="rounded-2xl bg-slate-100 w-fit h-[calc(100vh-3rem)] p-4">
         <h2 className="text-2xl font-semibold mb-6">SafePet</h2>
         <div className="flex flex-col gap-6">
-          <Link to={'/dashboard'}>Dashboard</Link>
+          <Link to={'/dashboard'} className="link">
+            <Home />
+            Dashboard
+          </Link>
           <NavbarCllinicSection />
-          <Link to={'/usuarios'}>Usuarios</Link>
+          <Link to={'/usuarios'} className="link">
+            <Users />
+            Usuarios
+          </Link>
         </div>
       </aside>
     </>
